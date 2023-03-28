1883eed60eac73c2b55465670dd2c42f506eae6a
ryansummerlin <ryansummerlin777@gmail.com>
2023-03-27 21:59:26 -0700

    Sent static .jpg and .css files from my local http server

--- server.js
+++ server.js
@@ -2,7 +2,29 @@ const http = require('http');
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // Your code here
+  console.log(req.method, req.url);
+  res.statusCode = 200;
+
+  if (req.url.startsWith('/static')) {
+    let parsedURL = req.url.split('/');
+    let extension = parsedURL[3].split('.')[1];
+
+    if (extension === 'css') {
+      res.setHeader('Content-Type', 'text/css');
+      const cssFile = fs.readFileSync(`./assets/css/${parsedURL[3]}`);
+      return res.end(cssFile);
+    }
+
+    if (extension === 'jpg') {
+      res.setHeader('Content-Type', 'image/jpeg');
+      const dogImage = fs.readFileSync(`./assets/images/${parsedURL[3]}`);
+      return res.end(dogImage);
+    }
+  }
+
+  const index = fs.readFileSync('./index.html');
+  res.setHeader('Content-Type', 'text/html');
+  return res.end(index);
 });
 
 const port = 5000;
